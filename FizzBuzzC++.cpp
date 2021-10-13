ab057e8b8a5f43c411d8e6afe0e925763d785969
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2021-10-13 16:44:06 +0200

    Update FizzBuzzC++.cpp

--- FizzBuzzC++.cpp
+++ FizzBuzzC++.cpp
@@ -7,18 +7,15 @@ int main()
         if (i % 3 == 0 && i % 5 == 0)
         {
             std::cout << "FizzBuzz\n";
-        }
-        
+        }        
         else if (i % 3 == 0)
         {
             std::cout << "Fizz\n";
-        }
-        
+        }        
         else if (i % 5 == 0)
         {
             std::cout << "Buzz\n";
-        }
-       
+        }       
         else
         {
             std::cout << i << "\n";
